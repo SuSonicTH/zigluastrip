9825ff602490f614d36c25bddff3b4adf13ed3d5
Michael Wolf <susonicth@gmail.com>
2023-07-29 22:47:10 +0200

    added commandline handling and file reading/writing

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,86 @@
 const std = @import("std");
 
 pub fn main() !void {
-    //const source = @embedFile("ftcsv.lua");
-    const source = @embedFile("serpent.lua");
-    try luastrip(source);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len == 3) {
+        try luastrip_file(args[1], args[2], allocator);
+    } else {
+        try std.io.getStdOut().writer().writeAll("\nusage: zigluastrip <input_file> <output_file>\n");
+    }
+}
+
+pub fn luastrip_file(input: [:0]const u8, output: [:0]const u8, allocator: std.mem.Allocator) !void {
+    const input_file = try std.fs.cwd().openFile(input, .{});
+    defer input_file.close();
+
+    const file_size = (try input_file.stat()).size;
+    const data = try allocator.alloc(u8, file_size);
+    defer allocator.free(data);
+    _ = try input_file.reader().readAll(data);
+
+    const stripped = try luastrip(data, allocator);
+    defer allocator.free(stripped);
+
+    const output_file = try std.fs.cwd().createFile(output, .{});
+    defer output_file.close();
+
+    try output_file.writer().writeAll(stripped);
+}
+
+pub fn luastrip(source: []u8, allocator: std.mem.Allocator) ![:0]const u8 {
+    var iterator: Tokenizer = .{
+        .data = source,
+    };
+    iterator.peek = iterator.next_token();
+
+    const buffer = try allocator.alloc(u8, source.len + 1);
+    errdefer allocator.free(buffer);
+    var pos: usize = 0;
+
+    var token = iterator.next();
+    while (token[0] != 0) : (token = iterator.next()) {
+        std.mem.copyForwards(u8, buffer[pos..], token);
+        pos += token.len;
+        switch (token[0]) {
+            ' ', '{', '}', '(', ')', '.', '+', '-', '*', '/', '"', '\'', '[', ']', '\n', '#', '=', ',', '~' => {
+                if (iterator.peek[0] == ' ') {
+                    _ = iterator.next();
+                }
+            },
+            else => {
+                switch (iterator.peek[0]) {
+                    ' ', '{', '}', '(', ')', '.', '+', '-', '*', '/', '"', '\'', '[', ']', '\n', '#', '=', ',', '~' => {},
+                    else => {
+                        buffer[pos] = ' ';
+                        pos += 1;
+                    },
+                }
+            },
+        }
+    }
+
+    buffer[pos] = 0;
+    pos += 1;
+    const ret = try allocator.realloc(buffer, pos);
+    return ret[0 .. pos - 1 :0];
 }
 
 const Tokenizer = struct {
-    data: [:0]const u8 = undefined,
+    data: []u8 = undefined,
     pos: usize = 0,
-    peek: []const u8 = "\n",
+    peek: []const u8 = " ",
 
     pub fn next(self: *Tokenizer) []const u8 {
         const token = self.peek;
         self.peek = self.next_token();
-        if (token[0] == '\n') {
-            while (self.peek[0] == '\n') {
+        if (token[0] == ' ') {
+            while (self.peek[0] == ' ') {
                 self.peek = self.next_token();
             }
         }
@@ -38,7 +103,7 @@ const Tokenizer = struct {
                 while (self.pos < self.data.len - 1 and (self.data[self.pos] == '\r' or self.data[self.pos] == '\n')) {
                     self.pos += 1;
                 }
-                return "\n";
+                return " ";
             },
             '-' => {
                 if (self.pos < self.data.len - 2 and self.data[self.pos + 1] == '-') {
@@ -118,40 +183,3 @@ const Tokenizer = struct {
         unreachable;
     }
 };
-
-fn tokenizer(source: [:0]const u8) Tokenizer {
-    var iterator: Tokenizer = .{
-        .data = source,
-    };
-    iterator.peek = iterator.next_token();
-    return iterator;
-}
-
-pub fn luastrip(source: [:0]const u8) !void {
-    const writer = std.io.getStdOut().writer();
-    var it: Tokenizer = tokenizer(source);
-    var token = it.next();
-    while (token[0] != 0) : (token = it.next()) {
-        if (token[0] == '\n') {
-            try writer.writeAll(" ");
-        } else {
-            try writer.writeAll(token);
-        }
-
-        switch (token[0]) {
-            '{', '}', '(', ')', '.', '+', '-', '*', '/', '"', '\'', '[', ']', '\n', '#', '=', ',', '~' => {
-                if (it.peek[0] == '\n') {
-                    _ = it.next();
-                }
-            },
-            else => {
-                switch (it.peek[0]) {
-                    '{', '}', '(', ')', '.', '+', '-', '*', '/', '"', '\'', '[', ']', '\n', '#', '=', ',', '~' => {},
-                    else => {
-                        try writer.writeAll(" ");
-                    },
-                }
-            },
-        }
-    }
-}
